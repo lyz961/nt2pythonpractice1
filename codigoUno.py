8616b5d6c308f72a499948a9592062ca2145f7c1
lyz961 <61318998+lyz961@users.noreply.github.com>
2023-08-11 11:11:33 -0500

    nt2 se crea nueva variable al proyect

--- codigoUno.py
+++ codigoUno.py
@@ -1,4 +1,4 @@
 nombreUsuario= input("digite su nombre")
 direccionUsuario= input("digite su direccion")
 edad= int(input("digite su edad"))
-
+cantidadDeHijos= int(input("digite la cantidad de hijos"))
